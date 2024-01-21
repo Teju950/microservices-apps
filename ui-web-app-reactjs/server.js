38ffc1ff078570313c04d139237014a1b96075a5
Teju950 <tejasri.tejub@gmail.com>
2024-01-21 22:55:29 +0530

    initial commit

--- ui-web-app-reactjs/server.js
+++ ui-web-app-reactjs/server.js
@@ -1,45 +1,31 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 const request = require('request');
+const cors = require('cors');
 
-var backendApiUrl=''
+// Use environment variable or default to 8080
+const PORT = process.env.PORT || 8080;
 
-function getBackendApiUrl(){
-    if(!backendApiUrl){
-        request('https://geolocation-db.com/json/344ec440-6bfc-11eb-a0c0-b5dee9e67313', { json: true }, (err, res, body) => {
-            if (err) { return console.log(err); }
-            console.log(JSON.stringify(body));
-            console.log(body.IPv4);
-            backendApiUrl = body.IPv4
-            return backendApiUrl;
-            console.log('Backend IP set to : '+body.IPv4);
-        });
-    }
-}
-getBackendApiUrl()
+// Use environment variable or default backend URL
+const backendApiUrl = process.env.BACKEND_API_URL || 'https://geolocation-db.com/json/344ec440-6bfc-11eb-a0c0-b5dee9e67313';
 
-
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/ping', (req, res) => {
-  return res.send('pong')
-})
+  return res.send('pong');
+});
 
 app.get('/myip', (req, res) => {
-    // return res.send(''+backendApiUrl)
-    return res.json({ url: ''+backendApiUrl });
-})
-
-
+    return res.json({ url: '' + backendApiUrl });
+});
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`UI App Started...`);
     console.log(`Node Server is running on port ${PORT}.`);
 });
